84305a4ec25400fa57dcb2fe51838a626f35ea87
Haris Naletakis <codenale@gmail.com>
2021-10-01 17:18:51 +0300

    New component for card item in listing of characters

---
+++ src/components/CardItem.js
@@ -0,0 +1,57 @@
+// Base imports
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// UI Components
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity
+} from 'react-native';
+
+// Styles
+import Styles from './CardItem.style';
+import { colors } from '../styles/index';
+
+
+const CardItem = ({ item, firstSeenIn, onCardItemPress }) => {
+    let statusColor;
+    if (item.status === 'Alive') {
+        statusColor = colors.green;
+    } else if (item.status === 'Dead') {
+        statusColor = colors.red;
+    } else {
+        statusColor = colors.light_gray;
+    }
+    return (
+        <TouchableOpacity onPress={onCardItemPress}>
+            <View style={Styles.container}>
+                <Image
+                    style={Styles.image}
+                    resizeMode={'cover'}
+                    source={{ uri: item.image }}
+                />
+                <View style={Styles.infoContainer}>
+                    <Text style={Styles.name}>{item.name}</Text>
+                    <View style={Styles.statusContainer}>
+                        <View style={[Styles.statusCircle, { backgroundColor: statusColor }]} />
+                        <Text style={Styles.statusText}>{`${item.status} - ${item.species}`}</Text>
+                    </View>
+                    <Text style={Styles.label}>Last known location:</Text>
+                    <Text style={Styles.info}>{item.origin.name}</Text>
+                    <Text style={Styles.label}>First seen in:</Text>
+                    <Text style={Styles.info}>{firstSeenIn}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+CardItem.propTypes = {
+    item: PropTypes.object, // TODO shape the object
+    firstSeenIn: PropTypes.string,
+    onCardItemPress: PropTypes.func
+};
+
+export default CardItem;
---
+++ src/components/CardItem.style.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native';
+import { fonts, colors, SPACE_X_SMALL, SPACE_SMALL, SPACE_XX_SMALL, SPACE_LARGE } from '../styles/index';
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: colors.gray_card,
+        marginBottom: 20,
+        borderRadius: 8,
+        flexDirection: 'row',
+        marginHorizontal: SPACE_XX_SMALL
+    },
+    image: {
+        flex: 0.4,
+        borderBottomLeftRadius: 8,
+        borderTopLeftRadius: 8
+    },
+    infoContainer: {
+        flex: 0.6,
+        flexDirection: 'column',
+        paddingStart: SPACE_SMALL,
+        paddingVertical: SPACE_SMALL,
+        paddingEnd: SPACE_SMALL
+    },
+    name: {
+        fontSize: fonts.size.xxlarge,
+        fontFamily: fonts.Roboto.Bold,
+        color: colors.less_white
+    },
+    statusContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    statusText: {
+        fontSize: fonts.size.medium,
+        fontFamily: fonts.Roboto.Medium,
+        color: colors.less_white,
+        paddingStart: SPACE_XX_SMALL
+    },
+    statusCircle: {
+        width: 8,
+        height: 8,
+        borderRadius: 8
+    },
+    label: {
+        fontSize: fonts.size.small,
+        fontFamily: fonts.Roboto.Regular,
+        color: colors.light_gray,
+        paddingTop: SPACE_SMALL
+    },
+    info: {
+        fontSize: fonts.size.medium,
+        fontFamily: fonts.Roboto.Medium,
+        color: colors.less_white,
+        paddingTop: SPACE_XX_SMALL
+    }
+});
